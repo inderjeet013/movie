50ea9dfeb24b630c6287ff76655a153c68c9ea09
inderjeet singh <39231175+inderjeet013@users.noreply.github.com>
2020-09-20 18:55:53 +0530

    Update recommend.js

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = 'a1914f88448bb1401eb6bf072297c7c5';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = 'a1914f88448bb1401eb6bf072297c7c5';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
